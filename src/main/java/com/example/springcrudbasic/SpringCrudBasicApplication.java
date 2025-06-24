ffb36477ce4fe87e6aa181f0cbca19f364479808
Heojamong <wlaud0823@naver.com>
2025-06-24 16:28:46 +0900

    feat : project init

---
+++ src/main/java/com/example/springcrudbasic/SpringCrudBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcrudbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCrudBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcrudbasic/SpringCrudBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcrudbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
